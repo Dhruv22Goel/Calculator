e74a0cbbef17e5cb5dc73a3c9baaed9e9c7e7a5b
Dhruv22Goel <91088068+Dhruv22Goel@users.noreply.github.com>
2022-08-12 17:54:56 +0530

    Add files via upload

---
+++ DigitButton.jsx
@@ -0,0 +1,12 @@
+import { ACTIONS } from "./App.jsx";
+import React from "react";
+function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  );
+}
+export default DigitButton;
